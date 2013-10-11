133963294cdf9b91e1d0977b8e2ac16add9b6c3e
raina lopez <raina_lopez@dalton.org>
2013-10-11 11:54:33 -0400

    made popall

--- datastructures/IntStack.java
+++ datastructures/IntStack.java
@@ -1,62 +1,80 @@
-import java.util.Random;
 public class IntStack {
     int [] stack;
     int top;
-    
     public IntStack(int size) {
 	top=0;
 	stack = new int[size];
+	
+
     }
     boolean isEmpty() {
 	return top==0; //if top==0 returns true else returns false  
     }
 
     void push(int num) {
-	stack[top++]=num; //stack=top at num and then it adds 1 to top so that way top moves over one and the next item that is pushed will be put behind the next available space 
+	stack[top++]=num; //stack=top at num and then it adds 1 to top so that way top moves over one and the next item that is pushed will be put into the next available space 
     }
 
     int pop() {
-	return stack[--top]; //subtracts 1 from top so you can see it
+	return stack[--top]; //subtracts 1 from top so that it is in the nearest filled space and returns what is in that space and then when the next item is pushed it overrides what is in that space 
     }
-    int peek() {
+    int peek() {  
 	return stack[top-1]; //looks behind one space in the stack 
     }
-    /*
-     * Done by Sam it takes an int, which is inputed by the user
-     * It returns the Int at the depth inputted
-     */
-    int peekDepth(int num) {
-	return stack[top-num]; //looks behind a selected space
+
+    //issue #2
+    // http://stackoverflow.com/questions/1519736/random-shuffling-of-an-array
+    int[] randomize(){
+
+	int[] list = new int[top];
+	for (int i=0; i< list.length; i++)
+	    {
+		//make the array
+		list[i] = pop();
+		//shuffle the array
+		shuffleArray(list);
+		//redo the array
+		for (int j=0; j<list.length;j++)
+		    {
+			return list[j];
+		    } 
+	    }
+	return list;
+    }   
+
+    //issue #10   
+    //by raina
+    int[] popall(){
+	//make an array
+	int[] list = new int[top];
+	for (int i=0; i< list.length; i++)
+	    {
+		list[i] = pop();
+	    } 
+	//return the array
+	return list;
     }
-    int peekRDepth() {
-	Random generator = new Random();
-	int num = generator.nextInt(top);
-	return stack[top-num]; //looks behind a random inputted space
-    }
-    int popRDepth() {
-	Random generator = new Random();
-	int num = generator.nextInt(top);
-	int x = stack[top-num];
-	stack[top-num] = 0;
-	return x;
-    }
-    void pushRDepth(int num) {
-	Random generator = new Random();
-	int num2 = generator.nextInt(top);
-	stack[top - num2] = num;
-    }
+
     public static void main(String[] args) {
 	IntStack is = new IntStack(10);
 	is.push(4);
 	is.push(5);
 	is.push(6);
-
-
-	System.out.println("The number at 3 is " + is.peekDepth(3));
+	
 	int k = is.pop();
 	System.out.println(k);
-	System.out.println("at a random depth it is " + is.peekRDepth());
-	System.out.println("this is the output at a random Depth " + is.popRDepth());
-	is.pushRDepth(9);
+	System.out.println();
+
+	//test for randomize
+	int[] test = is.randomize();
+	for (int j : test) System.out.println(j+" ");
+	System.out.println("\n");
+
+
+	//tests for popall:
+        int[] tmpArr = is.popall();
+	for (int i : tmpArr) System.out.print(i + " ");
+	System.out.println("\n");
     }
+
 }
