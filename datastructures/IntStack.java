3729b71012ea8b03112cce90606b5d4a2c35af43
Oliver Freiberg <oliver_freiberg@dalton.org>
2013-10-11 12:16:22 -0400

    reverse function, Oliver Freiberg.  Takes the stack and reverses the order that the stack is in.

--- datastructures/IntStack.java
+++ datastructures/IntStack.java
@@ -5,31 +5,48 @@ public class IntStack {
     public IntStack(int size) {
 	top=0;
 	stack = new int[size];
-	
-
     }
     boolean isEmpty() {
 	return top==0; //if top==0 returns true else returns false  
     }
 
     void push(int num) {
-	statck[top++]=num; //stack=top at num and then it adds 1 to top so that way top moves over one and the next item that is pushed will be put into the next available space 
+	stack[top++]=num; //stack=top at num and then it adds 1 to top so that way top moves over one and the next item that is pushed will be put into the next available space 
     }
 
     int pop() {
 	return stack[--top]; //subtracts 1 from top so that it is in the nearest filled space and returns what is in that space and then when the next item is pushed it overrides what is in that space 
     }
     int peek() {  
-	retun stack[top-1]; //looks behind one space in the stack 
+	return stack[top-1]; //looks behind one space in the stack 
     }
 
+    void reverse() {
+
+	int[] thing = new int[top];
+	for (int p = 0; p <= thing.length; p++) {
+	    thing[p] = pop();
+	}
+	for (int p : thing) {
+	    push(p);
+	    System.out.println(p);
+	}
+    }
+    
+    
     public static void main(String[] args) {
-	IntStack is = new IntStack(10);
+	IntStack is = new IntStack(4);
 	is.push(4);
 	is.push(5);
-	is.push(6);
+       	is.push(6);
+	is.push(7);
+	//int x = Integer.parseInt(args[0]);
 	int k = is.pop();
 	System.out.println(k);
+        
+
+
+	//	System.out.println(is.pop());
     }
 
 }
